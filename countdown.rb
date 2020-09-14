3531a4885869bcee938f7a279d54ab2400c7189a
Michael Murray <cloud19912003@live.com>
2020-09-14 22:41:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,5 +4,6 @@ def countdown(number)
   while number > 0 
     puts "#{number} SECOND(S)!"
   end
-  number == 0 ? (puts "HAPPY NEW YEAR!") :
+  number == 0 ? (puts "HAPPY NEW YEAR!")
+  "HAPPY NEW YEAR!"
 end
